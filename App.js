2861ebe141072788c53d83dedd99e0a272b7033c
Abdullah ALTUNKAYNAK <58576152+abdullah-altunkaynak@users.noreply.github.com>
2022-08-16 14:02:44 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+//Abdullah Altunkaynak 16.08.2022
+import React from "react";
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+//components
+import SearchBar from './src/components/SearchBar/SearchBar'
+import CardItem from './src/components/CardItem/CardItem'
+//data
+import productData from './src/data/product-data.json'
+
+export default function App() {
+  const renderProducts = ({ item }) => <CardItem products={item} />
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>PATIKASTORE</Text>
+            <FlatList
+                numColumns={2}
+                ListHeaderComponent={() => (<SearchBar />)}
+                keyExtractor={item => item.id.toString()}
+                data={productData}
+                renderItem={renderProducts}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 40,
+  },
+  title:{
+    fontWeight: 'bold',
+    fontSize: 35,
+    textAlign: 'left',
+    color: 'purple'
+  }
+});
